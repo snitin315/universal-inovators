82cf76cf372a1b098c63432602bb166370e16602
Nitin Kumar <snitin315@gmail.com>
2019-09-14 18:44:52 +0530

    updated fdp details

--- src/views/Fdp/Details.js
+++ src/views/Fdp/Details.js
@@ -44,8 +44,8 @@ export default function Details() {
           <CardBody  style = {design}>
              <Fade cascade>
              
-             <strong style={card}>1 One Week FDP (National/International).</strong> <hr/>
-             <strong style={card}>2 Two Week FDP (National/International).</strong> <hr/>
+             <strong style={card}>1 One Week FDP (National/International)</strong> <hr/>
+             <strong style={card}>2 Two Week FDP (National/International)</strong> <hr/>
              <strong style={card}>3 Workshops</strong> <hr/>
              <strong style={card}>4 Seminars</strong> <hr/>
             
