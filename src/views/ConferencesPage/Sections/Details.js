58a344602987f0f2ab017fdd90ab11103b13c944
Nitin Kumar <snitin315@gmail.com>
2019-10-22 19:31:14 +0530

    Updated Conference-pag user interface

--- src/views/ConferencesPage/Sections/Details.js
+++ src/views/ConferencesPage/Sections/Details.js
@@ -4,12 +4,14 @@ import { makeStyles } from "@material-ui/core/styles";
 
 
 import sideImage from "assets/img/conf2.jpg"
+import gif from "assets/img/Conf.gif"
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import InfoArea from "components/InfoArea/InfoArea.js";
 import Fade from 'react-reveal/Fade'
 import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
+import NavPills from "components/NavPills/NavPills.js";
 import { SupervisorAccount } from "@material-ui/icons";
 
 const useStyles = makeStyles(styles);
@@ -51,51 +53,84 @@ export default function Details() {
           </GridContainer>
       
       <div>
-      <h4  className={classes.title}>Some Of The Details Of The Conferences Are As Follows:</h4>
-        <GridContainer>
+      <h4 className={classes.title}>OUR CONFERENCES</h4>  
+      <h4  className={classes.description}>Some Of The Details Of The Conferences Are As Follows:</h4>
+      <NavPills
+      color="info"
+      tabs={[
+        {
+          tabButton: "Conference 2018",
+          tabContent: (
+            <GridContainer style = {flex}>
+            <GridItem xs={12} sm={12} md={3}>
+              <Fade left duration={2000}>
+               <img style = {{width : "100%"}} src={gif}/>
+               </Fade> 
+            </GridItem>    
+  
+            <GridItem xs={12} sm={12} md={9}>
+  
+           
+            <p className={classes.description} style={design} >
+              <strong>468 papers received</strong> | <strong>  92 papers accepted </strong> <br/><br/>
+              First International Conference on Innovative Computing and Communications (ICICC-2018) has been organized at Guru Nanak Institute of Management, New Delhi, India on 5-6th May 2018. We received 468 papers and accepted 92 papers with an acceptance ratio of 19%. All accepted papers has already been published by Springer in its reputed Scopus indexed Lecture Notes on Networks and Systems (LNNS). We received papers from 20 countries and got lot of recommendations from the science community for the organization and the quality of papers.
+            </p>
+          
+  
+            </GridItem>
+            </GridContainer>
+          )
+        },
+        {
+          tabButton: "Conference 2019",
+          tabContent: (
+            <GridContainer style = {flex}>
+            <GridItem xs={12} sm={12} md={3}>
+              <Fade left duration={2000}>
+               <img style = {{width : "100%"}} src={gif}/>
+               </Fade> 
+            </GridItem>    
+  
+            <GridItem xs={12} sm={12} md={9}>
+  
+           
+            <p className={classes.description} style={design} >
+              <strong>552 papers received</strong> | <strong>  125 papers accepted </strong> <br/><br/>
+              Second version of International Conference on Innovative Computing and Communications (ICICC-2019) has been organized at VSB-Technical University of Ostrava, Czech Republic on 21-22nd March 2019 with a pre-conference symposium at New Delhi, India on 16th March 2019. We received around 552 papers and accepted 125 papers with an acceptance ratio of 22%. All accepted papers will be published by Springer in its reputed scopus indexed AISC. We received papers from 25 countries
+            </p>
+          
+  
+            </GridItem>
+            </GridContainer>
+            
+          )
+        },
+        {
+          tabButton: "Conference 2020 ",
+          tabContent: (
+            <GridContainer style = {flex}>
+            <GridItem xs={12} sm={12} md={3}>
+              <Fade left duration={2000}>
+               <img style = {{width : "100%"}} src={gif}/>
+               </Fade> 
+            </GridItem>    
+  
+            <GridItem xs={12} sm={12} md={9}>
+  
+           
+            <p className={classes.description} style={design} >
+              <strong>643 papers received</strong> | <strong>  134 papers accepted </strong> <br/><br/>
+              The third version of ICICC-2020 will be held at Shaheed Sukhdev College of Business Studies, University of Delhi, New Delhi on 21-23rd February,2020 and it is jointly organised by Shaheed Sukhdev College of Business Studies, University of Delhi and National Institute of Technology Patna. All the accepted papers (after double blinded peer review) are published by Springer AISC series (ISI Proceedings, EI-Compendex, DBLP, SCOPUS, Google Scholar and Springerlink) and extended selected papers will be published in the special issues of SCI/SCOPUS/WoS/DBLP/ACM indexed Journals.
+            </p>
+          
+  
+            </GridItem>
+            </GridContainer>
+          )
+        }
+      ]}
+    />
         
-          <GridItem xs={12} sm={12} md={4}>
-          <Fade left>
-            <InfoArea
-              title="1st Conference"
-              description="First International Conference on Innovative Computing and Communications (ICICC-2018) has been organized at Guru Nanak Institute of Management, New Delhi, India on 5-6th May 2018. We received 468 papers and accepted 92 papers with an acceptance ratio of 19%. All accepted papers has already been published by Springer in its reputed Scopus indexed Lecture Notes on Networks and Systems (LNNS). We received papers from 20 countries and got lot of recommendations from the science community for the organization and the quality of papers."
-              icon={SupervisorAccount}
-              iconColor="primary"
-              vertical
-            />
-             </Fade>
-          </GridItem>
-         
-
-          
-          <GridItem xs={12} sm={12} md={4}>
-          <Fade left delay={500}>
-            <InfoArea
-              title="2nd Conference"
-              description="Second version of International Conference on Innovative Computing and Communications (ICICC-2019) has been organized at VSB-Technical University of Ostrava, Czech Republic on 21-22nd March 2019 with a pre-conference symposium at New Delhi, India on 16th March 2019. We received around 552 papers and accepted 125 papers with an acceptance ratio of 22%. All accepted papers will be published by Springer in its reputed scopus indexed AISC. We received papers from 25 countries"
-              icon={SupervisorAccount}
-              iconColor="primary"
-              vertical
-            />
-            </Fade>
-          </GridItem>
-          
-
-          
-          <GridItem xs={12} sm={12} md={4}>
-          <Fade left delay={1000}>
-            <InfoArea
-              title="3rd Conference"
-              description="The third version of ICICC-2020 will be held at Shaheed Sukhdev College of Business Studies, University of Delhi, New Delhi on 21-23rd February,2020 and it is jointly organised by Shaheed Sukhdev College of Business Studies, University of Delhi and National Institute of Technology Patna. All the accepted papers (after double blinded peer review) are published by Springer AISC series (ISI Proceedings, EI-Compendex, DBLP, SCOPUS, Google Scholar and Springerlink) and extended selected papers will be published in the special issues of SCI/SCOPUS/WoS/DBLP/ACM indexed Journals."
-              icon={SupervisorAccount}
-              iconColor="primary"
-              vertical
-            />
-            </Fade>
-          </GridItem>
-         
-        </GridContainer>
-
         <h5 className={classes.title}>Web-Link For The Conference : <a href="http://icicc-conf.com/" target="_blank">icicc-conf.com</a></h5>
        
       </div>
