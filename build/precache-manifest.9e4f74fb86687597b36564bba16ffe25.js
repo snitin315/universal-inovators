d0b8e0e6ee96b112d5f8899945327bcebcc84092
Nitin Kumar <snitin315@gmail.com>
2019-10-27 11:13:05 +0530

    remover build from gitignore

---
+++ build/precache-manifest.9e4f74fb86687597b36564bba16ffe25.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6968586d31f9e0f1a40acc0cc81da08c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941e3a14bd40c3cfd246",
+    "url": "/static/css/main.af1924df.chunk.css"
+  },
+  {
+    "revision": "a11c4a167150d9d55b6b",
+    "url": "/static/js/2.9c0ed5c4.chunk.js"
+  },
+  {
+    "revision": "941e3a14bd40c3cfd246",
+    "url": "/static/js/main.3476ae14.chunk.js"
+  },
+  {
+    "revision": "4931b02d894689682610",
+    "url": "/static/js/runtime~main.a033672e.js"
+  },
+  {
+    "revision": "f2901a55117cae9235bd3f4e9d27dafa",
+    "url": "/static/media/Conf.f2901a55.gif"
+  },
+  {
+    "revision": "316ef5cfa71360e9bda09a283c747465",
+    "url": "/static/media/NDA.316ef5cf.jpg"
+  },
+  {
+    "revision": "eba0f0f47c7949f63b10efa78de97157",
+    "url": "/static/media/book1.eba0f0f4.png"
+  },
+  {
+    "revision": "f23619274742f3720f6718504128a899",
+    "url": "/static/media/book2.f2361927.png"
+  },
+  {
+    "revision": "08475d2f8fa78b1cd908c04694a1ff5d",
+    "url": "/static/media/book3.08475d2f.png"
+  },
+  {
+    "revision": "766720e8341ae0c2300421d8cb359ad3",
+    "url": "/static/media/book4.766720e8.png"
+  },
+  {
+    "revision": "67b2067d14b75e67add53fd40039ca70",
+    "url": "/static/media/books-page.67b2067d.jpeg"
+  },
+  {
+    "revision": "da705074801dd780bd8e23228fda2738",
+    "url": "/static/media/callogo.da705074.jpeg"
+  },
+  {
+    "revision": "c28d2142f6d68c7ce6a5b4cf9c970feb",
+    "url": "/static/media/catA.c28d2142.jpeg"
+  },
+  {
+    "revision": "b6c9db1c19fd3db626e2d6d0702d009e",
+    "url": "/static/media/catA2.b6c9db1c.jpeg"
+  },
+  {
+    "revision": "b7b1541b162983121d291cd9a4aa38dc",
+    "url": "/static/media/catA3.b7b1541b.jpeg"
+  },
+  {
+    "revision": "57b805a0f624c0837b10e0c4cc5b1c55",
+    "url": "/static/media/cloud.57b805a0.png"
+  },
+  {
+    "revision": "af62927eefb692e51953b6c50c641c2a",
+    "url": "/static/media/conf2.af62927e.jpg"
+  },
+  {
+    "revision": "aa7e4eeb425cf5517d59d600b480aa68",
+    "url": "/static/media/conference-page.aa7e4eeb.jpeg"
+  },
+  {
+    "revision": "91fb78186fcf3e65518044397a0c1ef1",
+    "url": "/static/media/cyber.91fb7818.jpg"
+  },
+  {
+    "revision": "82e4cb06848d4583a4867bcf1527bbc1",
+    "url": "/static/media/dilogo.82e4cb06.jpg"
+  },
+  {
+    "revision": "2dd869a30ec1c47e59a3bd507fb4f97e",
+    "url": "/static/media/dmrlogo.2dd869a3.jpeg"
+  },
+  {
+    "revision": "0ebc9667e3112ce02d5c16b5cd98cfa3",
+    "url": "/static/media/fdp2.0ebc9667.jpg"
+  },
+  {
+    "revision": "489fefb299ca79d61758f27788a199af",
+    "url": "/static/media/googleScholar.489fefb2.png"
+  },
+  {
+    "revision": "228f6d8702295fa33da667051860ad00",
+    "url": "/static/media/gvlogo.228f6d87.png"
+  },
+  {
+    "revision": "254418b6036584ae73432be1332aed1f",
+    "url": "/static/media/idea.254418b6.jpeg"
+  },
+  {
+    "revision": "a06cf66395097ce2c14d27e3dbada79f",
+    "url": "/static/media/iot.a06cf663.jpg"
+  },
+  {
+    "revision": "9b739e6ad803f7d7b4c41bc693432f60",
+    "url": "/static/media/journal.9b739e6a.jpg"
+  },
+  {
+    "revision": "b0bd715bab837832bfd401a98b9a4a5b",
+    "url": "/static/media/journals.b0bd715b.png"
+  },
+  {
+    "revision": "6ec27d7b2a773b9a0233e5b69a1839a2",
+    "url": "/static/media/journalsOverview3.6ec27d7b.png"
+  },
+  {
+    "revision": "1eff11bd5ee3393fa89e691e49f26a8f",
+    "url": "/static/media/member.1eff11bd.jpg"
+  },
+  {
+    "revision": "8ca86834725931b31af6e3922e605f7b",
+    "url": "/static/media/minlogo.8ca86834.png"
+  },
+  {
+    "revision": "99aaa6bb6457e13ab300be799a04143c",
+    "url": "/static/media/ml.99aaa6bb.jpeg"
+  },
+  {
+    "revision": "71ca116717545cdfd09ba938d88ea031",
+    "url": "/static/media/negdlogo.71ca1167.jpeg"
+  },
+  {
+    "revision": "fb5037d47e825897dd27286f2f55f672",
+    "url": "/static/media/nitplogo.fb5037d4.jpeg"
+  },
+  {
+    "revision": "09d8b4fa2c7b599df5d33767819763ec",
+    "url": "/static/media/npclogo.09d8b4fa.jpeg"
+  },
+  {
+    "revision": "abca1816112b000bc113dfcf693e5aa7",
+    "url": "/static/media/objectives.abca1816.svg"
+  },
+  {
+    "revision": "8823ce6f58fd2666a9d6c36d402890c1",
+    "url": "/static/media/patent-page.8823ce6f.jpeg"
+  },
+  {
+    "revision": "eb48b67b7e705bd159d5080c51df81bb",
+    "url": "/static/media/paymentTerms.eb48b67b.png"
+  },
+  {
+    "revision": "bb6fb11b918e8767a69713981a9b6dfe",
+    "url": "/static/media/peer.bb6fb11b.png"
+  },
+  {
+    "revision": "99dead9bd8e09c613caa3518a8e5de02",
+    "url": "/static/media/pkslogo.99dead9b.jpeg"
+  },
+  {
+    "revision": "c9d34655cd72939a441b4474f4affdab",
+    "url": "/static/media/plucklogo.c9d34655.jpeg"
+  },
+  {
+    "revision": "1a6ec37b8cb752c29f593aba04cb2ef8",
+    "url": "/static/media/recycle.1a6ec37b.jpg"
+  },
+  {
+    "revision": "671460b510808bf73422588ffff77d11",
+    "url": "/static/media/rtmlogo.671460b5.jpeg"
+  },
+  {
+    "revision": "3f813be7937e496ab477353005bc4040",
+    "url": "/static/media/school.3f813be7.jpg"
+  },
+  {
+    "revision": "51418c1afc78f86c35c4aa7f9f59b2b1",
+    "url": "/static/media/services.51418c1a.png"
+  },
+  {
+    "revision": "c1a80a9249fc7f36fddaf9f0e8ba7c02",
+    "url": "/static/media/session2.c1a80a92.png"
+  },
+  {
+    "revision": "f7cb305d31ae4fc68f1e09301ea7b523",
+    "url": "/static/media/setitlogo.f7cb305d.png"
+  },
+  {
+    "revision": "bf7ad37a9574cdcc414412d39c753d0e",
+    "url": "/static/media/sgrtlogo.bf7ad37a.jpeg"
+  },
+  {
+    "revision": "c0683f44c47bb4732d720e61e0cb98e1",
+    "url": "/static/media/side.c0683f44.svg"
+  },
+  {
+    "revision": "401990373d71aab7c2a92776a48682e4",
+    "url": "/static/media/solution.40199037.png"
+  },
+  {
+    "revision": "aa71dfed75302b55fa56eef6042f7897",
+    "url": "/static/media/spainlogo.aa71dfed.jpeg"
+  },
+  {
+    "revision": "15991ae2de77847b806e41df4bc6e66e",
+    "url": "/static/media/srgelogo.15991ae2.png"
+  },
+  {
+    "revision": "fb151acf4487b596269f58b4a55094ed",
+    "url": "/static/media/ssclogo.fb151acf.png"
+  },
+  {
+    "revision": "213bbcdf820a3a530cae26093004a61f",
+    "url": "/static/media/start-bg.213bbcdf.png"
+  },
+  {
+    "revision": "9c2247d04de7cada684a8d257ed68696",
+    "url": "/static/media/startup-about.9c2247d0.jpeg"
+  },
+  {
+    "revision": "7736c151203a9f8865d6be3eea524924",
+    "url": "/static/media/thinklogo.7736c151.jpeg"
+  },
+  {
+    "revision": "bbf247b98719f49af78b6cb6a70908a8",
+    "url": "/static/media/uilogo.bbf247b9.jpeg"
+  },
+  {
+    "revision": "5b1b4e060ffb07534b82ad3aabc4e5cf",
+    "url": "/static/media/uischool-page.5b1b4e06.jpeg"
+  },
+  {
+    "revision": "6840d39e97077d6de267b1394616c286",
+    "url": "/static/media/water.6840d39e.png"
+  },
+  {
+    "revision": "db48d7e2f9561f1d29459be617d50a48",
+    "url": "/static/media/workshops.db48d7e2.jpg"
+  }
+]);
