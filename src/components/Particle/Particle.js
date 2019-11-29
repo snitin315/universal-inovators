0d602cb61c75206d3b36e058ac0954cb437a0922
Nitin Kumar <snitin315@gmail.com>
2019-11-29 21:09:31 +0530

    add particle.js component

---
+++ src/components/Particle/Particle.js
@@ -0,0 +1,45 @@
+import React, {Component, Children} from 'react';
+import Particles from 'react-particles-js';
+
+
+const ParticlesContainer = (props) => {
+
+    const bg ={
+        top: "0px",
+        bottom: "0px",
+        right: "0px",
+        left: "0px",
+        position: "fixed",
+        zIndex: "-1",
+    }
+
+    return ( 
+        <Particles 
+            style ={bg}
+            params={{
+                "particles": {
+                    "number": {
+                        "value": 60
+                    },
+                    "size": {
+                        "value": 3
+                    }
+                },
+                "interactivity": {
+                    "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "repulse"
+                        }
+                    }
+                }
+                
+            }} >
+
+            {props.children}    
+
+        </Particles>        
+    )
+}
+
+export default ParticlesContainer ;
