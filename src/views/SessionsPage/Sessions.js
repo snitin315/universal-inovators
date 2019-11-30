5a415eb809dfbc6fec7a44a1738ae5bec872781e
Nitin Kumar <snitin315@gmail.com>
2019-11-30 20:19:09 +0530

    update about page

--- src/views/SessionsPage/Sessions.js
+++ src/views/SessionsPage/Sessions.js
@@ -13,7 +13,9 @@ import StartupHeader from "components/Header/StartupHeader";
 import Parallax from "components/Parallax/Parallax.js";
 import styles from "assets/jss/material-kit-react/views/landingPage.js";
 import Fade from 'react-reveal/Fade';
-
+import ParticlesContainer from "components/Particle/Particle.js"
+import Button from "components/CustomButtons/Button.js";
+import rocket from "assets/img/startup6.gif"
 
 // Sections for this page
 import CharPose from "../LandingPage/Sections/CharPose.js";
@@ -31,9 +33,9 @@ export default function StartUpPage(props) {
     display : 'flex',
     alignItems : 'center',
   } ;
-
-  const style={
-    color : 'cyan'
+  const btn ={
+    borderRadius: "25px",
+    border : "1px solid #fff"
   }
 
   return (
@@ -43,7 +45,7 @@ export default function StartUpPage(props) {
       <Header
         color="transparent"
         routes={dashboardRoutes}
-        brand="Universal Inovators"
+        brand="StartUp Meets Investors"
         rightLinks={<StartupHeader />}
         fixed
         changeColorOnScroll={{
@@ -53,22 +55,33 @@ export default function StartUpPage(props) {
         {...rest}
       />
       
-      <Fade duration = "1500"  >
-      <Parallax filter image={require("assets/img/startup-about.jpeg")}>
+      <Parallax filter responsive style={{backgroundColor:"#28a796"}}> 
+    <ParticlesContainer />
         <div className={classes.container}>
-          <GridContainer style = {flex}>
+          <GridContainer style={flex}>
             <GridItem xs={12} sm={12} md={6}>
-            
-              <h1 className={classes.title}  > <CharPose   text = "About UISI 2020" /></h1>
-             
+              <h2 className={classes.title}><CharPose text="Where StartUp Meet Investors" /></h2>
+              <h6 className={classes.title} style={{marginTop : "-10px"}}>Let your dreams come true || Your Story Starts with us</h6>
               <br />
-
+              <Button
+                style={btn}
+                color="white"
+                size="md"
+                href="/startup-application-form"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fas fa-pencil-alt" />
+               <b style={{color:"#888"}}>Register Today</b> 
+              </Button>
             </GridItem>
-
+            <GridItem xs={12} sm={12} md={6}>
+              <br/>
+              <img src ={rocket} style={{maxWidth:"100%", opacity:"0.8", borderRadius:" 100% 100% 0% 0%"}}/>
+            </GridItem>
           </GridContainer>
         </div>
-      </Parallax>
-      </Fade>
+    </Parallax> 
       
       <Fade bottom duration ={2000} delay="500">
       <div className={classNames(classes.main, classes.mainRaised)}>
