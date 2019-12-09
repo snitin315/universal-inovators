eab9132e51b70639906bb21c154adebfb12bec51
Nitin Kumar <snitin315@gmail.com>
2019-12-09 22:46:39 +0530

    add payfees modal

--- src/components/PayFees/PayFee.js
+++ src/components/PayFees/PayFee.js
@@ -14,8 +14,18 @@ import styles from "assets/jss/material-kit-react/views/landingPage.js";
 import Close from "@material-ui/icons/Close";
 // core components
 import Button from "components/CustomButtons/Button.js";
+import  Album from '@material-ui/icons/Album';
+import GridItem from 'components/Grid/GridItem';
+import GridContainer from 'components/Grid/GridContainer';
 
+const flex = {display : "flex", alignItems : "center",textalign : "justify"}
 
+function ListStyle(props){
+  const classes = useStyles(); 
+  return (
+    <h6 className={classes.description} style= {flex}><Album style = {{ color : "#28a796"}}/>  </h6>
+  )
+}
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="down" ref={ref} {...props} />;
@@ -48,30 +58,39 @@ export default function PayFees() {
           disableTypography
           className={classes.modalHeader}
         >
-          <IconButton
-            className={classes.modalCloseButton}
-            key="close"
-            aria-label="Close"
-            color="inherit"
-            onClick={() => setModal(false)}
-          >
-            <Close className={classes.modalClose} />
-          </IconButton>
-          <h4 className={classes.modalTitle}>Modal title</h4>
+          <h6 className={classes.modalTitle}>Pay via NEFT/RTGS</h6>
         </DialogTitle>
         <DialogContent
           id="modal-slide-description"
           className={classes.modalBody}
-        >
-          <h5>Are you sure you want to do this?</h5>
+          style={{marginTop : "-45px"}}
+        ><br/>
+          <h6><li style={{listStyle: "none", marginLeft : "5px" , textAlign : "left"}}>Account Name: ICICC </li></h6>
+          <h6><li style={{listStyle: "none", marginLeft : "5px" , textAlign : "left"}}>Account No: 510909010080523  </li></h6>
+          <h6><li style={{listStyle: "none", marginLeft : "5px" , textAlign : "left"}}>Bank: City Union Bank </li></h6>
+          <h6><li style={{listStyle: "none", marginLeft : "5px" , textAlign : "left"}}>Branch: Sector-8, Rohini, Delhi </li></h6>
+          <h6><li style={{listStyle: "none", marginLeft : "5px" , textAlign : "left"}}>IFSC Code: CIUB0000244 </li></h6>
+    
+          <GridContainer>
+              <GridItem xs={5} sm={5} md={5}>
+                <hr/>
+              </GridItem>
+              <GridItem xs={2} sm={2} md={2}>
+                 <b>OR</b>
+              </GridItem>
+              <GridItem xs={5} sm={5} md={5}>
+                  <hr/>
+              </GridItem> 
+          </GridContainer>
+          <Button color="info" size="md" round target="_blank" href="https://www.scupo.in/event/3rd-international-conference-on-innovative-computing-communication/#bookticket">
+            <i class="fas fa-ticket-alt"/>  
+            <b style={{color:"#fff"}}>Pay Registration Fees Online</b>
+          </Button>
+          
         </DialogContent>
         <DialogActions
           className={classes.modalFooter + " " + classes.modalFooterCenter}
         >
-          <Button onClick={() => setModal(false)}>Never Mind</Button>
-          <Button onClick={() => setModal(false)} color="success">
-            Yes
-          </Button>
         </DialogActions>
       </Dialog>
     </div>
