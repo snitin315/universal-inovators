785ce06bddb3759acb2984aa7331b0c63dec6bc4
Nitin Kumar <snitin315@gmail.com>
2019-12-11 15:25:18 +0530

    update EventTracks.js

--- src/views/StartUpPage/Sections/EventTracks.js
+++ src/views/StartUpPage/Sections/EventTracks.js
@@ -44,9 +44,9 @@ export default function EventTracks() {
             <strong><CardHeader color="info">Track 1</CardHeader>  </strong>     
             <CardBody>
             <h4 className={cards.cardTitle}> IDEA</h4>
-            <p className={cards.description}>
-            Discuss your business idea
-            </p>
+            <b className={cards.description}>
+            Discuss your idea
+            </b>
             </CardBody>
         </Card>
 
@@ -60,7 +60,7 @@ export default function EventTracks() {
             <strong><CardHeader color="info">Track 2</CardHeader>  </strong>     
             <CardBody>
             <h4 className={cards.cardTitle}>PROTOTYPE</h4>
-            <p className={cards.description}>  Demonstrate your idea  </p>
+            <b className={cards.description}>  Demonstrate your idea  </b>
             </CardBody>
         </Card>
 
@@ -74,9 +74,9 @@ export default function EventTracks() {
             <strong><CardHeader color="info">Track 3</CardHeader>  </strong>     
             <CardBody>
             <h4 className={cards.cardTitle}> PRODUCT </h4>
-            <p className={cards.description}>
+            <b className={cards.description}>
             Showcase your idea
-            </p>
+            </b>
             </CardBody>
         </Card>
 
@@ -90,9 +90,9 @@ export default function EventTracks() {
             <strong><CardHeader color="info">Track 4</CardHeader>  </strong>     
             <CardBody>
             <h4 className={cards.cardTitle}>START-UP</h4>
-            <p className={cards.description}>
+            <b className={cards.description}>
             Share your success story
-            </p>
+            </b>
             </CardBody>
         </Card>
 
